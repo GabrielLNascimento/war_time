8ef0bab7667dbcb67d311f9ae2415ba1f5ec3a40
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2024-07-01 18:18:53 -0300

    acessibilidade usuario

--- scripts/ataque.js
+++ scripts/ataque.js
@@ -3,11 +3,13 @@ import { trocar_tela, Personagem, caixa_coracoes } from './index.js';
 const golpe_fraco = document.querySelector('.golpe-fraco')
 const texto_acima = document.getElementById('texto')
 const span_vida = document.getElementById('span-vida-inimigo')
-const vida_inimigo_hp = parseFloat(span_vida.textContent)
+let vida_inimigo_hp = parseFloat(span_vida.textContent)
 let vida_inimigo_atual = 0
 const coracoes_atuais = document.querySelector('.vida-hero-local')
 let msg_usuario = document.querySelector('.msg-usuario')
 let quem_joga = true
+let first_atack = 0
+let vitoria = false
 
 
 function atacar_inicio() {
@@ -16,9 +18,13 @@ function atacar_inicio() {
     const inimigo_local = document.querySelector('.img-inimigo-local')
 
     trocar_tela('.container_home', '.atacar_container')
+    
     inimigo_local.src = '../imgs/personagens/level-1.png'
     caixa.style.display = 'flex'
-
+    span_vida.innerHTML = '100'
+    vida_inimigo_hp = 100
+    msg_usuario.innerHTML = ''
+    vida_inimigo_atual = 0
   
     
     if (Personagem[1] == 'guerreiro') {
@@ -31,7 +37,7 @@ function atacar_inicio() {
         console.log('Deu erro!')
     }
 
-    if (Personagem[2] == 'coracao-extra') {
+    if (Personagem[2] == 'coracao-extra' && first_atack == 0) {
         const novo_elemento = document.createElement('img')
         const caixa_vida = document.querySelector('.vida-hero-local')
 
@@ -39,8 +45,9 @@ function atacar_inicio() {
         novo_elemento.setAttribute('id', 'cora-6')
         novo_elemento.setAttribute('src', '../imgs/icones/coracao-extra.png')
         caixa_vida.appendChild(novo_elemento)
+        first_atack = 1;
     }
- 
+    
 }
 
 function jogar() {
@@ -81,8 +88,8 @@ function golpe_fraco_btn() {
     if (parseFloat(span_vida.textContent) <= 0) {
         msg_usuario.innerHTML = 'Você venceu'
         texto_acima.innerHTML = ' '
-
-        console.log(coracoes_atuais)
+        golpe_fraco.style.display = 'none'
+        vitoria = true
     }
    
 }
@@ -95,23 +102,30 @@ function inimigo() {
         const dano_hero = roletar()
         if ((dano_hero%2 == 0) || (dano_hero%3 == 0)) {
             msg_usuario.innerHTML = 'Inimigo errou o ataque'
-            tirar_coracao(caixa_coracoes)
             jogador()
            
         } else {
             msg_usuario.innerHTML = 'Inimigo acertou o ataque'
-            tirar_coracao(caixa_coracoes)
-            jogador()
+            let tira_cora = tirar_coracao(caixa_coracoes)
+            if (tira_cora == true) {
+                jogador()
+            }
+            
         }
         
 
-    }, 3000)
+    }, 2000)
 }
 
 function abandonar() {
-    trocar_tela('.atacar_container', '.container_home')
-
-    // colocar um alert mostrando quantas moedas ele perdeu
+    if (vitoria) {
+        
+        trocar_tela('.atacar_container', '.container_home')
+    } else {
+        trocar_tela('.atacar_container', '.container_home')
+        // colocar um alert mostrando quantas moedas ele perdeu
+    }
+    
 }
 
 function roletar() {
@@ -147,14 +161,45 @@ function tirar_coracao(caixa_coracoes) {
         novo_coracao2.setAttribute('alt', 'Coração Extra');
         coracoes_atuais.appendChild(novo_coracao2);
         console.log('coracao adicionado')
+        return true
     } else {
         console.log('Não há corações para remover.');
+        
     }
+
+   
+    const coracoes_verificados = verificar_coracoes()
+    if (coracoes_verificados == 0) {
+        golpe_fraco.style.display = 'none'
+        msg_usuario.innerHTML = 'Você perdeu!'
+        return false
+        // colocar um modo de aparecer na tela que morreu!
+        // tirar o botão de atacar e abandonar partida
+     
+    } 
+
+    
+}
+
+function verificar_coracoes() {
+    let soma = 0
+    let div_coracoes = document.querySelector('.vida-hero-local');
+    if (div_coracoes) {
+        let coracoes = div_coracoes.children;
+        for (let cora of coracoes) {
+            if (cora.id != 'coracao-cinza') {
+                soma += 1
+            }
+        }
+    } else {
+        console.log('Elemento .vida-hero-local não encontrado');
+    }
+
+    return soma
 }
 
 
 
 
 
-
-export { abandonar, atacar_inicio, jogador, inimigo, roletar, golpe_fraco_btn, jogar, tirar_coracao, coracoes_atuais }
+export { abandonar, atacar_inicio, jogador, inimigo, roletar, golpe_fraco_btn, jogar, tirar_coracao, coracoes_atuais, verificar_coracoes }
